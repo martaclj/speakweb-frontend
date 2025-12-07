9e5a2dd3973f3b1bcae94b335e494c630893e54f
martaclj <104948192+martaclj@users.noreply.github.com>
2025-12-07 10:54:43 +0100

    add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
+    // rutas públicas
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // si vacío, ir a login
-    { path: 'login', component: LoginComponent }, 
-    { path: 'home', component: HomeComponent }, 
+    { path: 'login', component: LoginComponent },
+    
+    // rutas protegidas
+    { path: 'home', component: HomeComponent, canActivate: [authGuard] }, 
+
 
 ];
