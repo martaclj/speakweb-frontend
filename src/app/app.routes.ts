3d75865e693329b4dfd6935bb6d7af852b6d6593
martaclj <104948192+martaclj@users.noreply.github.com>
2025-12-07 20:00:45 +0100

    my communities page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './guards/auth.guard';
+import { MyCommunitiesComponent } from './pages/my-communities/my-communities.component';
 
 export const routes: Routes = [
     // rutas públicas
@@ -10,6 +11,10 @@ export const routes: Routes = [
     
     // rutas protegidas
     { path: 'home', component: HomeComponent, canActivate: [authGuard] }, 
-
+    {
+        path: 'my-communities',
+        component: MyCommunitiesComponent,
+        canActivate: [authGuard]
+    }
 
 ];
---
+++ src/app/pages/my-communities/my-communities.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { GroupService } from '../../services/group.service';
+import { GroupMember } from '../../interfaces/group-member';
+
+@Component({
+  selector: 'app-my-communities',
+  imports: [],
+  templateUrl: './my-communities.component.html',
+  styleUrl: './my-communities.component.css'
+})
+export class MyCommunitiesComponent implements OnInit {
+
+  private groupService = inject(GroupService);
+  
+  // lista de mis grupos (comunidades)
+  myMemberships: GroupMember[] = [];
+  isLoading: boolean = true;
+
+  ngOnInit(): void {
+    this.groupService.getMyGroups().subscribe({
+      next: (data) => {
+        console.log("Mis comunidades cargadas:", data);
+        this.myMemberships = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error("Error al cargar mis comunidades:", err);
+        this.isLoading = false;
+      }
+    });
+  }
+
+
+}
