a05c13cb393e8c7855906ab7ac845d488de5eaed
martaclj <marlarrad@gmail.com>
2025-12-07 18:45:15 +0100

    enviorments and join group

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,14 +11,25 @@ export class GroupService {
   // inyección de dependencia
   private http = inject(HttpClient);
 
-  // url de mi API
-  private apiUrl ='http://localhost:8080/api/groups';
+  // url de mi API de la variable environment
+  private baseUrl = environment.apiUrl;
 
   constructor() { }
 
+  // GET: /api/groups
   // método get recupero todos los grupos disponibles
-  getGroups(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+  getAllGroups(): Observable<any[]> {
+    const url = `${this.baseUrl}/groups`;
+    return this.http.get<any[]>(url);
+  }
+
+  // POST: /api/group-members/join
+  // método para unirse a un grupo
+  joinGroup(groupIdreceived: number): Observable<any> {
+    const url = `${this.baseUrl}/group-members/join`;
+    const body = { groupId: groupIdreceived };
+
+    return this.http.post(url, body);
   }
 
 }
