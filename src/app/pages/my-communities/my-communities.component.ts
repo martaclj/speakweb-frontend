b563fe33245ff40267c3a65f2f30c552202458b5
martaclj <104948192+martaclj@users.noreply.github.com>
2025-12-08 08:31:57 +0100

    style adjustments

--- src/app/pages/my-communities/my-communities.component.ts
+++ src/app/pages/my-communities/my-communities.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { GroupService } from '../../services/group.service';
 import { GroupMember } from '../../interfaces/group-member';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-my-communities',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './my-communities.component.html',
   styleUrl: './my-communities.component.css'
 })
