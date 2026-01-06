1c2bbe99e57e5e882e3cdbadfeadb6ac21682379
martaclj <104948192+martaclj@users.noreply.github.com>
2026-01-06 17:40:19 +0100

    footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,15 @@
-import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'speakweb-frontend';
-  currentYear = new Date().getFullYear();
-
-  private router = inject(Router);
-
-  // // función para saber si login o registro y no enseñar footer
-  // showFooter(): boolean {
-  //   const currentUrl = this.router.url;
-  //   if (currentUrl === '/login' || currentUrl === '/register') {
-  //     return false;
-  //   } else {
-  //     return true;
-  //   }
-  // }
 }
