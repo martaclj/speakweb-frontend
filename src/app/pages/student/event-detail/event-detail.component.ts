ef2ab04815fa38ac87598d8a0baa51d743d4c3f1
martaclj <104948192+martaclj@users.noreply.github.com>
2026-01-13 00:42:08 +0100

    join button fix

--- src/app/pages/student/event-detail/event-detail.component.ts
+++ src/app/pages/student/event-detail/event-detail.component.ts
@@ -4,20 +4,26 @@ import { DatePipe } from '@angular/common';
 import { EventService } from '../../../services/event.service';
 import { GroupEvent } from '../../../interfaces/group-event';
 import { Group } from '../../../interfaces/group';
+import { GroupMemberService } from '../../../services/group-member.service';
+import { EventParticipantService } from '../../../services/event-participant.service';
 
 @Component({
   selector: 'app-event-detail',
-  imports: [DatePipe, RouterLink],
+  imports: [DatePipe, RouterLink],   // DatePipe para facilitar el trabajo con fechas
   templateUrl: './event-detail.component.html',
   styleUrl: './event-detail.component.css'
 })
-export class EventDetailComponent implements OnInit{
+export class EventDetailComponent {
   private route = inject(ActivatedRoute);
   private eventService = inject(EventService);
-  // DatePipe para facilitar el trabajo con fechas
+  private groupMemberService = inject(GroupMemberService);
+  private participantService = inject(EventParticipantService);
+
 
   event?: GroupEvent;
   isLoading: boolean = true;
+  isGroupMember: boolean = false;
+  isJoinedEvent: boolean = false;
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -32,11 +38,53 @@ export class EventDetailComponent implements OnInit{
     this.eventService.getEventById(id).subscribe({
       next: (data) => {
         this.event = data;
+
+        if (this.event) {
+          this.checkPermissions(this.event.group.id);
+          this.checkIfJoinedEvent(this.event.id);
+        }
         this.isLoading = false;
       },
       error: (err) => {
         console.error("Error al cargar evento:", err);
         this.isLoading = false;
+      }
+    });
+  }
+  checkPermissions(groupId: number) {
+    this.groupMemberService.getMyGroups().subscribe(myGroups => {
+      const found = myGroups.find(m => m.group.id === groupId);
+      if (found) {
+        this.isGroupMember = true;
+      } else {
+        this.isGroupMember = false;
+      }
+    });
+  }
+  checkIfJoinedEvent(eventId: number) {
+    this.participantService.getMyEvents().subscribe(myEvents => {
+      const found = myEvents.find((p: any) => p.event.id === eventId);
+      
+      if (found) {
+        this.isJoinedEvent = true;
+      } else {
+        this.isJoinedEvent = false;
+      }
+    });
+  }
+
+  joinEvent() {
+    if (!this.event) 
+      return;
+
+    this.participantService.joinEvent(this.event.id).subscribe({
+      next: () => {
+        alert('¡Te has apuntado correctamente!');
+        this.isJoinedEvent = true;
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Error al apuntarse');
       }
     });
   }
