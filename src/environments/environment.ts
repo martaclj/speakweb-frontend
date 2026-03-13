6e223aa42650208b6fff10035a69da0b301905f0
martaclj <marlarrad@gmail.com>
2026-03-13 22:48:57 +0100

    mejora botón + cambio https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // URL futura: apiUrl: 'https://speakweb.martaclj.com/api'
-    apiUrl: 'http://speakweb.martaclj.com/api',
-    serverUrl: 'http://speakweb.martaclj.com'
+    apiUrl: 'https://speakweb.martaclj.com/api',
+    serverUrl: 'https://speakweb.martaclj.com'
 };
 // ng serve --configuration production
