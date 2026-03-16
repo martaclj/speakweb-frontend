47a2c9e584da7d67ddb9374f0b9f63b17b12be87
martaclj <104948192+martaclj@users.noreply.github.com>
2026-03-16 21:09:45 +0100

    participant-card updated

--- src/app/components/participant-card/participant-card.component.ts
+++ src/app/components/participant-card/participant-card.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-participant-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './participant-card.component.html',
   styleUrl: './participant-card.component.css'
 })
